c9116a42d079d438224cc421be0633f7bce5ee76
David Mohl <git@davemail.io>
2022-04-06 19:19:10 +0900

    Move hooks from context to package root

--- saifu.d.ts
+++ saifu.d.ts
@@ -1,7 +1,35 @@
-import { TokenInfo } from '@solana/spl-token-registry';
-import { Connection, PublicKey } from '@solana/web3.js';
-import { FunctionComponent } from 'react';
-import { UseQueryResult } from 'react-query';
+import { TokenInfo } from "@solana/spl-token-registry";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { FunctionComponent } from "react";
+import { UseQueryResult } from "react-query";
+
+// Hooks
+export declare const useTokenAccounts: () => UseQueryResult<
+  Map<string, TokenAccount>,
+  unknown
+>;
+export declare const useNFTAccounts: () => UseQueryResult<
+  TokenAccount[],
+  unknown
+>;
+export declare const useTokenMetadata: (
+  addr: string
+) => UseQueryResult<TokenMetadata, unknown>;
+export declare const useConnection: () => Connection;
+export declare const usePublicKey: () => PublicKey | undefined;
+export declare const useTokenInfos: () => TokenInfo[];
+export declare const usePrice: (
+  tokenInfo?: TokenInfo | undefined
+) => UseQueryResult<number | undefined, unknown>;
+
+/**
+ * Fetches the given image by url from cache, or retrieves it
+ * If not found, will retrieve and cache for ttl seconds
+ */
+export declare const useCachedImage: (
+  url: string | undefined,
+  ttl?: number | undefined
+) => UseQueryResult<string | undefined, unknown>;
 
 interface Attribute {
   trait_type: string;
@@ -51,14 +79,40 @@ interface TokenMetadata {
  * to plugins
  */
 export interface AppContext {
+  /**
+   * @deprecated Import hooks directly from package
+   */
   hooks: {
+    /**
+     * @deprecated Import hooks directly from package
+     */
     useTokenAccounts: () => UseQueryResult<Map<string, TokenAccount>, unknown>;
+    /**
+     * @deprecated Import hooks directly from package
+     */
     useNFTAccounts: () => UseQueryResult<TokenAccount[], unknown>;
+    /**
+     * @deprecated Import hooks directly from package
+     */
     useTokenMetadata: (addr: string) => UseQueryResult<TokenMetadata, unknown>;
+    /**
+     * @deprecated Import hooks directly from package
+     */
     useConnection: () => Connection;
+    /**
+     * @deprecated Import hooks directly from package
+     */
     usePublicKey: () => PublicKey | undefined;
+    /**
+     * @deprecated Import hooks directly from package
+     */
     useTokenInfos: () => TokenInfo[];
-    usePrice: (tokenInfo?: TokenInfo | undefined) => UseQueryResult<number | undefined, unknown>;
+    /**
+     * @deprecated Import hooks directly from package
+     */
+    usePrice: (
+      tokenInfo?: TokenInfo | undefined
+    ) => UseQueryResult<number | undefined, unknown>;
   };
 }
 
@@ -71,13 +125,14 @@ export interface View {
   component: FunctionComponent<ViewProps>;
   icon?: React.ReactElement;
 }
+
 export declare abstract class PluginSettings {
   abstract plugin: Plugin;
-  constructor(app: AppContext, plugin: Plugin);
+  constructor(plugin: Plugin);
   abstract display(): Setting[];
 }
 export declare enum SettingElementType {
-  Text = 'text',
+  Text = "text",
 }
 export declare abstract class SettingsElement {
   abstract type: SettingElementType;
@@ -99,13 +154,44 @@ export declare class Setting {
   setDesc(desc: string): this;
   addText(func: (setting: TextSetting) => TextSetting): this;
 }
+
 export declare abstract class Plugin {
+  /**
+   * AppContext holds meta information about the wallet
+   */
   app: AppContext;
+
   constructor(app: AppContext);
-  abstract id: string;
+
+  /**
+   * Called when the plugin is getting loaded by the wallet
+   * Use this to initialize your plugin, register views, etc.
+   */
   abstract onload(): Promise<void>;
+
+  /**
+   * Adds the given view to the wallet
+   * Views are displayed in the Tile section of the wallet
+   * One Plugin can have multiple views
+   */
   addView(view: View): void;
+
+  /**
+   * Register PluginSettings to be associated with this plugin
+   * Setting a PluginSettings object will enable the settings
+   * menu to appear for your Plugin
+   */
   setSettings(settings: PluginSettings): void;
+
+  /**
+   * Load data from the plugin storage
+   * Returns a Promise that resolves with any data that
+   * was previously stored
+   */
   loadData(): Promise<any>;
+
+  /**
+   * Saves the given data in plugin storage
+   */
   saveData(data: any): Promise<void>;
 }
